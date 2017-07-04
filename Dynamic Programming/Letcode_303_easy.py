909b6e7d0f37e6a1d8c8a790e62f2b8b79001b20
lilitom <ahguohao11@163.com>
2017-07-04 10:21:59 +0800

    数组指定区间的和

--- Dynamic Programming/Letcode_303_easy.py
+++ Dynamic Programming/Letcode_303_easy.py
@@ -15,6 +15,8 @@ There are many calls to sumRange function.
 #Author:Guohao
 #coding=utf-8
 #from https://leetcode.com/problems/range-sum-query-immutable/#/description
+#------------------------------------------------------------------------------------
+#标准方法--牛 the best way-niu
 class NumArray(object):
     def __init__(self, nums):
         """
@@ -42,3 +44,35 @@ class NumArray(object):
 NumArray_=NumArray([-2, 0, 3, -5, 2, -1])
 print NumArray_.sumRange(0,-2)
 
+
+
+#------------------------------------------------------------------------------------
+#我的方法 my way
+#coding=utf-8
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.accu = [0]*(len(nums)+1)
+        for i in range(1,len(nums)+1):
+            self.accu[i]=self.accu[i-1]+nums[i-1]
+        print self.accu
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int 
+        :type j: int
+        :rtype: int 
+        """
+        return self.accu[j + 1] - self.accu[i]
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)
+NumArray_=NumArray([-2, 0, 3, -5, 2, -1])
+print NumArray_.sumRange(0,-2)
